9ec4672bea43ce1e5537d5372c7e610497e5fb93
Yoan <yoan.filipe@epsi.fr>
2018-09-26 10:31:18 +0200

    Création d'une librairie

---
+++ src/hello.cpp
@@ -0,0 +1,13 @@
+#include "hello.h"
+#include <iostream>
+
+void talk::hello(){
+	std::cout << "Hello word" << std::endl;
+}
+
+void talk::bonjour(){
+	std::cout << "Bonjour" << std::endl;
+}
+void talk::coucou(){
+	std::cout << "coucou" <<std::endl;
+}
---
+++ src/hello.h
@@ -0,0 +1,8 @@
+#pragma one
+
+class talk{
+public :
+	void hello();
+	void bonjour();
+	void coucou();
+};
