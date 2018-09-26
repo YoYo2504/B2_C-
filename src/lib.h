b277a47d061da255ab95eb804e00402a65dae392
Yoan <yoan.filipe@epsi.fr>
2018-09-26 12:50:54 +0200

    modification des commentaires dans le lib.h

--- src/lib.h
+++ src/lib.h
@@ -1,7 +1,7 @@
 /*! \file lib.h
-*\brief description
-*\author Prenom
-*\version
+*\brief description : systeme de calcul basique 
+*\author Yoan
+*\version 1
 *
 *
 *
@@ -16,22 +16,25 @@
 
 class calcul{
 public:
-/*! \brief description
-*\param nb1 description
-*\param nb2 description
+/*! \brief description : Addition
+*\param nb1 description : 10
+*\param nb2 description : 1
 */
 	int somme(int nb1, int nb2);
-/*!\brief description
-*\param nb3 description
-*\param nb4 description
+/*!\brief description : Soustraction
+*\param nb3 description : 10
+*\param nb4 description : 1
 */
 	int soustraction(int nb3,int nb4);
-/*!\brief description
-*\param nb5 description
-*\param nb6 description
+/*!\brief description : Multiplication
+*\param nb5 description : 10
+*\param nb6 description : 10
 */
 	int produit(int nb5,int nb6);
-
+/*!\brief description : Division 
+*\param nb7 description : 10
+*\param nb8 description : 10
+*/
 	int division(int nb7,int nb8);
 
 };
